a0659dcabd192be8b78586d3b24899ec7a4dcf8f
XuYongFan <54026630+XuYongFan@users.noreply.github.com>
2022-07-03 07:09:00 +0800

    Add files via upload

---
+++ pa.php
@@ -0,0 +1,13 @@
+<?php
+$id=$_GET['id'];
+$url = $id;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_NOBODY, 1); //不返回 BODY 页面内容
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //不直接输出
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); //返回最后的 Location
+curl_exec($ch);
+$info = curl_getinfo($ch,CURLINFO_EFFECTIVE_URL);
+curl_close($ch);
+header('Location:'.$info);
+?>
